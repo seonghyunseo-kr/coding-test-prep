708dd4a1575160244f71f3b7e4e8011ea90a9291
seonghyunseo-kr <seonghyunseo.kr@gmail.com>
2026-01-08 14:12:51 +0900

    Title: 수의 계절은, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 260108/수의 계절은/season-of-num.py
@@ -0,0 +1,10 @@
+M = int(input())
+
+if M >= 3 and M <= 5:
+    print('Spring')
+elif M >= 6 and M <= 8:
+    print('Summer')
+elif M >= 9 and M <= 11:
+    print('Fall')
+elif M >=12 or M <=2:
+    print('Winter')
