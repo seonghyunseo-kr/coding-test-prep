b358c213f46c61e30a10b7493ceae9ec55183318
seonghyunseo-kr <seonghyunseo.kr@gmail.com>
2026-01-08 14:31:02 +0900

    Title: 일 수 구하기, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

--- 260108/일 수 구하기/number-of-days-in-month.py
+++ 260108/일 수 구하기/number-of-days-in-month.py
@@ -1,11 +1,16 @@
 n =  int(input())
 
-if n % 2 != 0:
-    print(31)
-else:
-    if n == 2 :
-        print(28)
-    elif n == 8:
+if n <=8:
+    if n % 2 != 0 or n == 8:
         print(31)
     else:
+        if n == 2:
+            print(28)
+        else:
+            print(30)
+else:
+    if n % 2 != 0:
         print(30)
+    else:
+        print(31)
+    
