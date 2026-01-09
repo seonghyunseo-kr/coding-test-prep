d7eba94e7984806284930f776a98cd40dd8d5ca2
seonghyunseo-kr <seonghyunseo.kr@gmail.com>
2026-01-09 17:21:44 +0900

    Title: 조건에 부합하는 수, Time: 47ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 260109/조건에 부합하는 수/number-that-meets-the-condition.py
@@ -0,0 +1,10 @@
+A = int(input())
+
+for i in range(1, A+1):
+    if i % 2 == 0 and i % 4 != 0:
+        continue
+    if (i // 8) % 2 == 0:
+        continue
+    if i % 7 < 4:
+        continue
+    print(i, end=' ')
