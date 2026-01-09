fa2f1dfd7ba63eba61399b619d297537aa91f104
seonghyunseo-kr <seonghyunseo.kr@gmail.com>
2026-01-09 17:34:17 +0900

    Title: 1까지 나누기, Time: 45ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 260109/1까지 나누기/divide-by-1.py
+++ 260109/1까지 나누기/divide-by-1.py
@@ -1,13 +1,12 @@
 N = int(input())
 
-cnt = 0 
+cnt = 1
 for i in range(1, N+1):
     if N / i > 1:
         N = N // i
         cnt += 1
         continue
     else:
-        cnt += 1
         break
 
 print(cnt)
