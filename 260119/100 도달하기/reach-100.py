b65670b81c840a568fccb108af979d89c0581ace
seonghyunseo-kr <seonghyunseo.kr@gmail.com>
2026-01-19 12:55:02 +0900

    Title: 100 도달하기, Time: 43ms, Memory: 16MB, Status: Passed - Codetree

--- 260119/100 도달하기/reach-100.py
+++ 260119/100 도달하기/reach-100.py
@@ -1,13 +1,14 @@
 N = int(input())
 
+# 1. 초기 항 설정
 arr = [1, N]
-arr = arr + [0] * 7
 
-for i in range(2,9):
-    arr[i] = arr[i-2] + arr[i-1]
-    if arr[i] < 100:
-        continue
-    else:
-        break
+# 2. 마지막에 추가된 항(arr[-1])이 100 이하인 동안 계속 반복
+while arr[-1] <= 100:
+    # 전전항(arr[-2])과 전항(arr[-1])을 더해 새로운 값을 만듦
+    next_val = arr[-1] + arr[-2]
+    # 리스트에 추가
+    arr.append(next_val)
 
+# 3. 전체 수열 출력
 print(*arr)
