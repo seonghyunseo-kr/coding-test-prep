ce8e16ed67f80c9a93ad6859c950b87c58d6c1c9
seonghyunseo-kr <seonghyunseo.kr@gmail.com>
2026-01-22 10:29:12 +0900

    Title: 배열의 평균, Time: 49ms, Memory: 16MB, Status: Passed - Codetree

--- 260122/배열의 평균/ave-of-array.py
+++ 260122/배열의 평균/ave-of-array.py
@@ -5,14 +5,14 @@ for i in range(2):
     for j in range(4):
         sum_arr += arr[i][j]
     mean_arr = sum_arr/4
-    print(mean_arr, end=' ')
+    print(f"{mean_arr:.1f}", end=' ')
 print()
 for i in range(4):
     sum_arr = 0
     for j in range(2):
         sum_arr += arr[j][i]
     mean_arr = sum_arr / 2
-    print(mean_arr, end=' ')
+    print(f"{mean_arr:.1f}", end=' ')
 print()
 
 sum_arr = 0 
@@ -20,4 +20,4 @@ for i in range(2):
     for j in range(4):
         sum_arr += arr[i][j]
 mean_arr = sum_arr / 8 
-print(mean_arr)
+print(f"{mean_arr:.1f}")
