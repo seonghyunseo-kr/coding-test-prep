4905d48773f7ec33d725a6a07b2d544b449c0dac
seonghyunseo-kr <seonghyunseo.kr@gmail.com>
2026-01-24 23:08:52 +0900

    Title: 9-1 정사각형, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 260124/9-1 정사각형/9-1-square.py
@@ -0,0 +1,12 @@
+N = int(input())
+
+cnt = 9
+for i in range(N):
+    for j in range(N):
+        if cnt > 0:
+            print(cnt, end='')
+        else:
+            cnt = 9
+            print(cnt, end='')
+        cnt -= 1
+    print()
