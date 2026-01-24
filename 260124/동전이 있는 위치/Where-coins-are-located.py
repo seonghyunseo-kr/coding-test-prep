eacf5d232f1c412d8fec43963b1c71fecbb3f4b8
seonghyunseo-kr <seonghyunseo.kr@gmail.com>
2026-01-24 21:50:08 +0900

    Title: 동전이 있는 위치, Time: 47ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 260124/동전이 있는 위치/Where-coins-are-located.py
@@ -0,0 +1,10 @@
+N, M = map(int, input().split())
+placed = [[0 for _ in range(N)] for _ in range(N)]
+
+exist = False 
+for _ in range(M):
+    r, c = tuple(map(int, input().split()))
+    placed[r-1][c-1] = 1
+
+for row in placed:
+    print(*row)
