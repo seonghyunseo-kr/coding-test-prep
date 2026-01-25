b4278b3f34decdabc165cf6a319c9c0c9a6d8204
seonghyunseo-kr <seonghyunseo.kr@gmail.com>
2026-01-25 20:11:20 +0900

    Title: 2021년 날짜의 유무, Time: 47ms, Memory: 16MB, Status: Passed - Codetree

--- 260125/2021년 날짜의 유무/with-or-without-2021.py
+++ 260125/2021년 날짜의 유무/with-or-without-2021.py
@@ -10,17 +10,19 @@ def date(M, D):
             print('Yes')
         else:
             print('No')
-    elif M > 8:
+    elif M >= 8 and M < 13:
         if M % 2 == 0 and D < 32:
             print('Yes')
         elif M % 2 != 0 and D < 31:
             print('Yes')
         else:
             print('No')
-    else:
+    elif M == 2:
         if D < 29:
             print('Yes')
         else:
             print('No')
+    else:
+        print('No')
 
 date(M, D)
