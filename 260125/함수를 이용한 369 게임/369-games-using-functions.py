be26b69965d063fb0595820d22435b0ecf1cdbe7
seonghyunseo-kr <seonghyunseo.kr@gmail.com>
2026-01-25 17:38:13 +0900

    Title: 함수를 이용한 369 게임, Time: 44ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 260125/함수를 이용한 369 게임/369-games-using-functions.py
+++ 260125/함수를 이용한 369 게임/369-games-using-functions.py
@@ -1,13 +1,29 @@
 a, b = map(int, input().split())
 
-# Please write your code here.
-def printer(a, b):
-    cnt = 0 
-    for i in range(a, b+1):
-        prod = i // 10
-        remainder = i % 10 
-        if i % 3 == 0 or prod % 3 == 0 or remainder % 3 == 0:
-            cnt += 1
-    print(cnt)
+# 자릿수 별 숫자 나누는 함수
+def divider(a, b):
+    for num in range(a, b+1):
+        num = list(str(num))
+        cnt = 0
+        print('num:', num)
+        for i in num:
+            print('i:', i, end=' ')
+            if i == '3' or i == '6' or i == '9':
+                cnt += 1
+                print(cnt)
+    # print(cnt)
 
-printer(a, b)
+divider(a, b)
+
+# # 실제 문제 풀이하는 함수 - count
+# def printer(a, b):
+#     cnt = 0
+#     for i in range(a, b+1):
+#         if i % 3 == 0:
+#             cnt += 1
+#         else:
+#             if divider(a, b) == True:
+#                 cnt += 1
+#     print(cnt)
+
+# printer(a, b)
