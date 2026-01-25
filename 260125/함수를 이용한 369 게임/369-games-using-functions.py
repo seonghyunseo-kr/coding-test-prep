dba03cb235d14d759b44f7657f6cb48f9b5a622f
seonghyunseo-kr <seonghyunseo.kr@gmail.com>
2026-01-25 17:46:31 +0900

    Title: 함수를 이용한 369 게임, Time: 101ms, Memory: 17MB, Status: Passed - Codetree

--- 260125/함수를 이용한 369 게임/369-games-using-functions.py
+++ 260125/함수를 이용한 369 게임/369-games-using-functions.py
@@ -1,29 +1,21 @@
 a, b = map(int, input().split())
 
 # 자릿수 별 숫자 나누는 함수
-def divider(a, b):
+def counter(a, b):
+    total_cnt = 0
     for num in range(a, b+1):
-        num = list(str(num))
-        cnt = 0
-        print('num:', num)
-        for i in num:
-            print('i:', i, end=' ')
-            if i == '3' or i == '6' or i == '9':
-                cnt += 1
-                print(cnt)
-    # print(cnt)
+        if num % 3 != 0:
+            num = list(str(num))
+            div_cnt = 0
+            for i in num:
+                if i == '3' or i == '6' or i == '9':
+                    div_cnt += 1
+            if div_cnt > 0:
+                total_cnt += 1 
+        else:
+            total_cnt += 1
 
-divider(a, b)
+    print(total_cnt)
 
-# # 실제 문제 풀이하는 함수 - count
-# def printer(a, b):
-#     cnt = 0
-#     for i in range(a, b+1):
-#         if i % 3 == 0:
-#             cnt += 1
-#         else:
-#             if divider(a, b) == True:
-#                 cnt += 1
-#     print(cnt)
+counter(a, b)
 
-# printer(a, b)
