fa1a102afad31a6144d296b2ded7d2d8d0496c04
seonghyunseo-kr <seonghyunseo.kr@gmail.com>
2026-01-25 20:09:49 +0900

    Title: 2021년 날짜의 유무, Time: 46ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 260125/2021년 날짜의 유무/with-or-without-2021.py
+++ 260125/2021년 날짜의 유무/with-or-without-2021.py
@@ -3,15 +3,24 @@ M, D = map(int, input().split())
 # Please write your code here.
 
 def date(M, D):
-    if M < 13 and D < 32:
-        if M != 2:
+    if M < 8 and M != 2:
+        if M % 2 == 0 and D < 31:
+            print('Yes')
+        elif M % 2 != 0 and D < 32:
             print('Yes')
         else:
-            if D < 29:
-                print('Yes')
-            else:
-                print('No')
+            print('No')
+    elif M > 8:
+        if M % 2 == 0 and D < 32:
+            print('Yes')
+        elif M % 2 != 0 and D < 31:
+            print('Yes')
+        else:
+            print('No')
     else:
-        print('No')
+        if D < 29:
+            print('Yes')
+        else:
+            print('No')
 
 date(M, D)
