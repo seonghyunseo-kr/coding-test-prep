746d2260649f8ff0fca10a72d101eb15d51dc7b0
seonghyunseo-kr <seonghyunseo.kr@gmail.com>
2026-01-27 13:14:27 +0900

    Title: 1이 되는 순간까지, Time: 43ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 260127/1이 되는 순간까지/until-the-moment-I-reach-one.py
@@ -0,0 +1,13 @@
+N = int(input())
+
+# Please write your code here.
+def program(N):
+    if N <= 2:
+        return 0
+
+    if N % 2 == 0:
+        return program(N // 2) + 1
+    else:
+        return program(N // 3) + 1
+
+print(program(N))
