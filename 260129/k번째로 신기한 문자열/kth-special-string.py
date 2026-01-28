844607a42de97987c55d10b8c6a3d054d3271c57
seonghyunseo-kr <seonghyunseo.kr@gmail.com>
2026-01-29 00:08:52 +0900

    Title: k번째로 신기한 문자열, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

--- 260129/k번째로 신기한 문자열/kth-special-string.py
+++ 260129/k번째로 신기한 문자열/kth-special-string.py
@@ -5,9 +5,9 @@ str = [input() for _ in range(n)]
 # Please write your code here.
 arr = []
 for word in str:
-    if word in str:
+    if word[0:len(t)] == t:
         arr.append(word)
 
 sorted_arr = sorted(arr)
-print(sorted_arr[k])
+print(sorted_arr[k-1])
 
