169137addd78ea9ccce898ff53e604800da5b60f
seonghyunseo-kr <seonghyunseo.kr@gmail.com>
2026-01-29 16:30:35 +0900

    Title: 비오는 날, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

--- 260129/비오는 날/rainy-day.py
+++ 260129/비오는 날/rainy-day.py
@@ -15,8 +15,6 @@ class WeatherInfo:
         self.date = date
         self.day = day
         self.weather = weather
-        self.y, self.m, self.d = self.date.split('-')
-
         
 weather_list = []
 for i in range(n):
@@ -28,24 +26,10 @@ for i in range(n):
     if weather_list[i].weather == 'Rain':
         rainy_day.append(weather_list[i])
 
-min_date_y = rainy_day[0].y
-min_date_m = rainy_day[0].m
-min_date_d = rainy_day[0].d
-min_date = rainy_day[0]
+min_date_obj = rainy_day[0]
 
 for i in range(1, len(rainy_day)):
-    if min_date_y > rainy_day[i].y:
-        min_date_y = rainy_day[i].y
-        min_date = rainy_day[i]
+    if rainy_day[i].date < min_date_obj.date:
+        min_date_obj = rainy_day[i]
 
-    elif min_date_y == rainy_day[i].y:
-        min_date_y = rainy_day[i].y
-        if min_date_m > rainy_day[i].m:
-            min_date = rainy_day[i]
-
-        elif min_date_m == rainy_day[i].m:
-            if min_date_d > rainy_day[i].d:
-                min_date = rainy_day[i]
-        
-print(min_date.date, min_date.day, min_date.weather)
-    
+print(f"{min_date_obj.date} {min_date_obj.day} {min_date_obj.weather}")
