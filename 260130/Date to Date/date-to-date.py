aa179e3183c060870fb970faba8501d22bf1cefc
seonghyunseo-kr <seonghyunseo.kr@gmail.com>
2026-01-30 23:13:27 +0900

    Title: Date to Date, Time: 42ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 260130/Date to Date/date-to-date.py
@@ -0,0 +1,19 @@
+m1, d1, m2, d2 = map(int, input().split())
+
+# Please write your code here.
+dates = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+elapsed_dates = 1
+while True:
+    if m1 == m2 and d1 == d2:
+        break
+    
+    elapsed_dates += 1
+    d1 += 1
+
+    if d1 > dates[m1]:
+        m1 += 1
+        d1 = 1
+
+print(elapsed_dates)
+    
