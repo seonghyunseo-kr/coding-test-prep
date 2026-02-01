954b911e0684ebda0e3afeea2a6bf599e84bcb55
seonghyunseo-kr <seonghyunseo.kr@gmail.com>
2026-02-01 14:32:47 +0900

    Title: 흰검 칠하기, Time: 75ms, Memory: 19MB, Status: Runtime Error - Codetree

---
+++ 260201/흰검 칠하기/painting-white-black.py
@@ -0,0 +1,44 @@
+n = int(input())
+OFFSET = 7
+blocks = [0] * (OFFSET * 2 + 1)
+
+b_cnt = [0] * (OFFSET * 2 + 1)
+w_cnt = [0] * (OFFSET * 2 + 1)
+g_cnt = [0] * (OFFSET * 2 + 1)
+
+order = [0] * (OFFSET * 2 + 1)
+
+curr = OFFSET
+for _ in range(n):
+    x, dir = input().split()
+    x = int(x)
+
+    if dir == 'R':
+        start, end = curr, curr + x - 1
+        for i in range(start, end+1):
+            blocks[i] += 1 
+            b_cnt[i] += 1 
+            order[i] = 'B'
+        curr = end
+    else:
+        start, end = curr - x + 1, curr
+        for i in range(start, end+1):
+            blocks[i] += 1
+            w_cnt[i] += 1
+            order[i] = 'W'
+        curr = start
+    
+for i in range(len(blocks)):
+    if blocks[i] >= 4 :
+        if b_cnt[i] >= 2 and w_cnt[i] >= 2:
+            order[i] = 'G'
+
+w, b, g = 0, 0, 0
+for o in order:
+    if o == 'W':
+        w += 1
+    elif o == 'B':
+        b += 1
+    elif o == 'G':
+        g += 1
+print(w, b, g)
