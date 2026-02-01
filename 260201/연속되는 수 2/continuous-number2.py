a35142f7f511cfcabc5827f58b05ae88444e1cc5
seonghyunseo-kr <seonghyunseo.kr@gmail.com>
2026-02-01 20:53:51 +0900

    Title: 연속되는 수 2, Time: 45ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 260201/연속되는 수 2/continuous-number2.py
+++ 260201/연속되는 수 2/continuous-number2.py
@@ -3,10 +3,13 @@ arr = [int(input()) for _ in range(n)]
 
 # Please write your code here.
 max_cnt = 0
-cnt = 0
+cnt= 0
 for i in range(n):
+
     if i == 0 or arr[i] != arr[i-1]:
         cnt += 1
+    else:
+        cnt = 1
     
     if cnt > max_cnt:
         max_cnt = cnt
