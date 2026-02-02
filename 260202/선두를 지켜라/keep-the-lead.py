7c059350f22c31665cbf3f1a95a2a1f1fa037d5d
seonghyunseo-kr <seonghyunseo.kr@gmail.com>
2026-02-02 14:50:21 +0900

    Title: 선두를 지켜라, Time: 83ms, Memory: 20MB, Status: Runtime Error - Codetree

--- 260202/선두를 지켜라/keep-the-lead.py
+++ 260202/선두를 지켜라/keep-the-lead.py
@@ -21,7 +21,7 @@ for _ in range(m):
 total_time = curr_time 
 
 leader, ans = 0, 0 # if leader == 1: A is leading / leader == 2: B is leading 
-for i in range(total_time):
+for i in range(1, total_time):
     if a_blocks[i] > b_blocks[i]:
         if leader == 2:
             ans += 1
