d4baa1cc15d3bea9a8e6f8f8f01082f9e97b987d
seonghyunseo-kr <seonghyunseo.kr@gmail.com>
2026-02-05 23:24:55 +0900

    Title: 문자에 따른 명령 2, Time: 50ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 260205/문자에 따른 명령 2/text-based-commands2.py
+++ 260205/문자에 따른 명령 2/text-based-commands2.py
@@ -3,14 +3,14 @@ dirs = input()
 # Please write your code here.
 dirs = list(dirs)
 dx, dy = [1, 0, -1, 0], [0, -1, 0, 1]
-dir_num = 1
+dir_num = 3
 x, y = 0, 0
 for i in range(len(dirs)):
     if dirs[i] == 'L':
         dir_num = (dir_num + 1) % 4
     elif dirs[i] == 'R':
         dir_num = (dir_num + 3) % 4
-    else:
+    else: # dir = 'F'
         x, y = x + dx[dir_num], y + dy[dir_num]
 
 print(x, y)
