ca1283bb2cc304d2d731d18dcae6da2b5386eacf
seonghyunseo-kr <seonghyunseo.kr@gmail.com>
2026-02-10 16:04:55 +0900

    Title: 인접하지 않은 2개의 수, Time: 47ms, Memory: 17MB, Status: Passed - Codetree

--- 260210/인접하지 않은 2개의 수/two-non-adjacent-numbers.py
+++ 260210/인접하지 않은 2개의 수/two-non-adjacent-numbers.py
@@ -7,8 +7,8 @@ for i in range(n):
     for j in range(i+2, n):
         sub_sum = 0 
         sub_sum += numbers[i] + numbers[j]
-
-    if sub_sum > max_sum:
-        max_sum = sub_sum
         
+        if sub_sum > max_sum:
+            max_sum = sub_sum
+            
 print(max_sum)
