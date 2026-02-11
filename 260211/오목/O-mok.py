2941edbcfd9cf552b17550b8b993167d252f311b
seonghyunseo-kr <seonghyunseo.kr@gmail.com>
2026-02-11 14:20:23 +0900

    Title: 오목, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

--- 260211/오목/O-mok.py
+++ 260211/오목/O-mok.py
@@ -1,64 +1,42 @@
 board = [list(map(int, input().split())) for _ in range(19)]
 
-# Please write your code here.
+# 가로, 세로, 우하향, 우상향
+dr, dc = [0, 1, 1, -1], [1, 0, 1, 1]
 
-def checker(i, j):
-    row, row_cnt = False, 0 # 가로 방향
-    col, col_cnt = False, 0 # 세로 방향
-    cross, cross_cnt = False, 0 # 대각선 방향
+def in_range(r, c):
+    return 0 <= r < 19 and 0 <= c < 19
 
-    # 가로 방향 체크 (오른쪽 방향만)
-    for k in range(j+1, len(board)-1):
-        if board[i][j] == board[i][k] and board[i][k] == board[i][k+1]:
-            row_cnt += 1
-        else:
-            row_cnt = 0 
-        if row_cnt == 5:
-            print(board[i][k], 'win')
-            row = True 
-            break
-
-    # 세로 방향 체크 (아래쪽 방향만)
-    for l in range(i+1, len(board)-1):
-        if board[i][j] == board[l][j] and board[l][j] == board[l+1][j]:
-            col_cnt += 1
-        else:
-            col_cnt = 0 
-        if col_cnt == 5:
-            print(board[i][k], 'win')
-            col = True 
-            break 
-
-    # 대각선 방향 체크 (오른쪽-아래 방향만)
-    for k in range(j+1, len(board)-1):
-        for l in range(i+1, len(board)-1):
-            if board[i][j] == board[l][k] and board[l][k] == board[l+1][k+1] and l == k:
-                cross_cnt += 1
-            else:
-                cross_cnt = 0 
-            if cross_cnt == 5:
-                print(board[i][k], 'win')
-                cross = True 
-                break 
-    return row, col, cross
-
-for i in range(len(board)):
-    for j in range(len(board)):
-        if board[i][j] != 0: # 현재 위치 i, j 
-
-            row, col, cross = checker(i, j) # 현재 위치 기준으로 이후 승리 여부 확인
-            print(row, col, cross)
-
-            # 첫번째 줄 - 승리 여부 출력 
-            # 두번째 줄 - 연속된 5개 바둑알 중 가운데 위치하고 있는 가로 번호, 세로 번호 
-            if row == False and col == False and cross == False: #승리 결정되지 않은 경우 
-                print(0)
-            else: 
-                if board[i][j] == 1: # 검정이 이긴 경우 
-                    print(1)
-                    print(i+2, j+2) # 연속된 5개 바둑알 중 가운데 위치하고 있는 가로 번호, 세로 번호 
-                elif board[i][j] == 2: # 흰색이 이긴 경우 
-                    print(2)
-                    print(i+2, j+2) # 연속된 5개 바둑알 중 가운데 위치하고 있는 가로 번호, 세로 번호 
+def find_winner():
+    for r in range(19):
+        for c in range(19):
+            if board[r][c] == 0:
+                continue
             
+            color = board[r][c]
 
+            for i in range(4):
+                # 전 칸 확인 - 6목이 되는 경우가 있는지 확인 
+                prev_r, prev_c = r - dr[i], c - dc[i]
+                if in_range(prev_r, prev_c) and board[prev_r][prev_c] == color:
+                    continue
+
+                cnt = 1
+                nr, nc = r + dr[i], c + dc[i]
+                # 앞 칸 확인 - 계속 같은 색이 나올때까지 반복 
+                while in_range(nr, nc) and board[nr][nc] == color:
+                    cnt += 1
+                    nr += dr[i]
+                    nc += dc[i]
+
+                # 정확히 같은 색 반복이 5번인 경우만 승리 인정 / 승리한 색, 가운데 좌표 출력 
+                if cnt == 5:
+                    mid_r = r + dr[i] * 2
+                    mid_c = c + dc[i] * 2
+                    return color, mid_r + 1, mid_c + 1
+                    
+    return 0, -1, -1
+
+ans, win_r, win_c = find_winner()
+print(ans)
+if ans != 0:
+    print(win_r, win_c)
